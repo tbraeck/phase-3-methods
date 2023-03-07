51eeab4142c59be9b281cb34c20d01c4ed85b664
Tate <tatebraeckel@gmail.com>
2023-03-06 18:33:58 -0700

    commit tree 13eac67ba4be12fc23e4e802180f9103f9f77be6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+
+  def greet_programmer 
+    puts "Hello, programmer!"
+  end
+
+def greet( name)
+    puts "Hello, #{name}!"
+end
+#  greet "Naureen"
+#  greet "Jimmy"
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+
+# greets_with_default "Sunny"
+
+def add(num1, num2)
+     num1 + num2
+end
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+def halve(num)
+return nil unless num.class == Integer
+
+num/2
+end
